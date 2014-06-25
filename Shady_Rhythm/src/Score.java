e9100ef750dc5e5d68dfe61ee4872f81c6ab626e
DivineEnder <qwertydraw@gmail.com>
2014-06-25 01:55:36 -0700

    Primative Points System Added
    I have completed the primative points system. Basically it is a percentage system whereby the more circles you hit, the better your percentage is for the song. Each circle you do not hit decrements you song percentage.

---
+++ Shady_Rhythm/src/Score.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+
+public class Score
+{
+	float points;
+	float maxPoints;
+	float percentage;
+	
+	Score()
+	{
+		points = 0;
+		maxPoints = 0;
+		percentage = 1;
+	}
+	
+	public void updateMaxPoints()
+	{
+		maxPoints++;
+		percentage = points/maxPoints;
+	}
+	
+	public void modPoints(int value)
+	{
+		points += value;
+	}
+	
+	public void draw(Graphics g, GameContainer gc)
+	{
+		g.setColor(Color.cyan);
+		g.draw(new Rectangle(gc.getScreenWidth() - 100, 20, 50, 200));
+		g.fill((new Rectangle(gc.getScreenWidth() - 100, 20 + (200 * (1 - percentage)), 50, 200 * (percentage))));
+		g.setColor(Color.black);
+		g.drawString(Integer.toString((int)(percentage * 100)) + "%", gc.getScreenWidth() - 95, 30);
+	}
+}
